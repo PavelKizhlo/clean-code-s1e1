41c16c4c923da2a2cb443c681c24e352fa4cfbc2
Pavel Kizhlo <pavelkizh@gmail.com>
2022-05-26 17:23:34 +0300

    refactor: use class selectors instead of tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
 
+    listItem.className='list-item';
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
@@ -33,18 +34,20 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="checkbox";
     editInput.type="text";
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className='delete-image';
     deleteButton.appendChild(deleteButtonImg);
 
 
